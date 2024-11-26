c342b90b123cfba01a84fcb221b2e54a9925c313
Mario <artes.dev@uchile.cl>
2024-11-26 17:40:49 -0300

    edit item de tabla

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react'
 
 // Components
-import Layout from '../components/Layout'
 import Table from '../components/Table'
+import Layout from '../components/Layout'
+import EditForm from '../components/EditForm'
+
+// Services
 import { getComputers } from '../services/computer-service'
 
 const Home = () => {
 
-    const [ computers, setComputers ] = useState([])
-    const [ loading, setLoading ] = useState(true)
     const [ error, setError ] = useState(null)
+    const [ loading, setLoading ] = useState(true)
+    const [ computers, setComputers ] = useState([])
+    const [ isModalOpen, setIsModalOpen ] = useState(false)
+    const [ selectedItem, setSelectedItem ] = useState(null)
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,7 +29,25 @@ const Home = () => {
             }
         }
         fetchData()
-    }, [])
+    }, [computers] )
+
+    const handleEditClick = ( item ) => {
+        setSelectedItem( item )
+        setIsModalOpen(true)
+    }
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false)
+        setSelectedItem(null)
+    }
+
+    const handleUpdate = ( updatedComputer ) => {
+        setComputers( ( prevComputers ) =>
+            prevComputers.map((computer) =>
+                computer.id === updatedComputer.id ? { ...item, ...updatedItem } : computer
+            )
+        )
+    }
 
     if ( loading ) return <div>Loading...</div>
 
@@ -35,7 +59,14 @@ const Home = () => {
                 <h2 className="text-2xl font-bold mb-4">Lista de Etiquetas</h2>
                 <p className="mb-4">.-.</p>
                 <div className="bg-white p-6 shadow rounded">
-                    <Table data={ computers }/>
+                    <Table data={ computers } onEditClick={handleEditClick} />
+                    { isModalOpen && (
+                        <EditForm
+                            item = { selectedItem }
+                            setIsOpen = { handleCloseModal }
+                            onUpdate = { handleUpdate }
+                        />
+                    )}
                 </div>
             </div>
         </Layout>
--- src/services/computer-service.js
+++ src/services/computer-service.js
@@ -5,19 +5,19 @@ import {
     updateDoc,
     deleteDoc,
     collection,
-} from "firebase/firestore";
+} from "firebase/firestore"
 
-import { db } from "../firebase/firebase-config";
+import { db } from "../firebase/firebase-config"
 
-    const addComputer = async (computer) => {
-        console.log('comp', computer);
+    const addComputer = async ( computer ) => {
+        console.log('comp', computer)
         try {
-          const docRef = await addDoc(collection(db, "pc_tag"), computer);
-          console.log('Document written with ID: ', docRef.id);
-          alert('Document with Id: ' + docRef.id + ' has been created');
+          const docRef = await addDoc(collection(db, "pc_tag"), computer)
+          console.log('Document written with ID: ', docRef.id)
+          alert('Document with Id: ' + docRef.id + ' has been created')
         } catch (error) {
-          console.error('Error adding document: ', error);  // Usa console.error para más detalles
-          alert('Error adding document');
+          console.error('Error adding document: ', error)
+          alert('Error adding document')
         }
       }
       
@@ -25,10 +25,10 @@ import { db } from "../firebase/firebase-config";
 const getComputerById = async ( computerId ) => {
     try {
         const computerDoc = doc( db, "pc_tag", computerId )
-        const computer = await getDoc( computerDoc )
+        const computer = await getDocs( computerDoc )
         if ( computer.exist() ) return computer.data()
     } catch ( error ) {
-        
+        console.error('Computer not Found: ', error)
     }
 }
 
@@ -51,22 +51,23 @@ const getComputers = async () => {
 const updateComputer = async ( computerId, updatedComputer ) => {
     try {
         const computer = doc(db, "pc_tag", computerId)
-        await updateDoc( computer, updatedComputer )
+        await updateDoc(computer, updatedComputer)
 
-        alert('Computer with Id: ', computerId, 'has been updated')
+        alert('Computer with Id: ' + computerId + ' has been updated')
     } catch ( error ) {
-        
+        console.error("Error updating computer: ", error)
+        alert('Error updating computer: ' + error.message)
     }
 }
 
 const deleteComputer = async ( computerId ) => {
     try {
-        console.log('Id', computerId);
+        console.log('Id', computerId)
         
         const computer = doc(db, "pc_tag", computerId)
         await deleteDoc(computer )
     } catch ( error)  {
-        throw new Error("Error to delete computer", error);
+        throw new Error("Error to delete computer", error)
 
     }
 }
